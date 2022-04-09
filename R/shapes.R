77c9defdc7860e20a192673ab544612293016af7
eshom <48328872+eshom@users.noreply.github.com>
2022-04-09 19:08:25 +0300

    Updated R level shapes functions

--- R/shapes.R
+++ R/shapes.R
@@ -41,3 +41,92 @@ draw_pixel_v <- function(vector2_position, color) {
         .Call("DrawPixelV_R", vector2_position, color)
         invisible()
 }
+
+#' @family Basic shapes drawing functions
+#' @title Draw a line
+#' @param position Integer Vector with 4 components:
+#' x start position, y start_position, x end position, y end position
+#' @param color Color object. Integer vector with RGBA values between 0 and 255.
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+draw_line <- function(position, color) {
+        .Call("DrawLine_R", position, color)
+        invisible()
+}
+
+#' @family Basic shapes drawing functions
+#' @title Draw a line (Vector version)
+#' @param start_pos Vector2. Start position.
+#' @param end_pos Vector2. End position.
+#' @param color Color object. Integer vector with RGBA values between 0 and 255.
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+draw_line_v <- function(start_pos, end_pos, color) {
+        .Call("DrawLineV_R", start_pos, end_pos, color)
+        invisible()
+}
+
+#' @family Basic shapes drawing functions
+#' @title Draw a line defining thickness
+#' @param start_pos Vector2. Start position.
+#' @param end_pos Vector2. End position.
+#' @param thick Float value defining thickness.
+#' @param color Color object. Integer vector with RGBA values between 0 and 255.
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+draw_line_ex <- function(start_pos, end_pos, thick, color) {
+        .Call("DrawLineEx_R", start_pos, end_pos, color)
+        invisible()
+}
+
+#' @family Basic shapes drawing functions
+#' @title Draw a line using cubic-bezier curves in-out
+#' @param start_pos Vector2. Start position.
+#' @param end_pos Vector2. End position.
+#' @param thick Float value defining thickness.
+#' @param color Color object. Integer vector with RGBA values between 0 and 255.
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+draw_line_bezier <- function(start_pos, end_pos, thick, color) {
+        .Call("DrawLineBezier_R", start_pos, end_pos, thick, color)
+        invisible()
+}
+
+#' @family Basic shapes drawing functions
+#' @title Draw line using quadratic bezier curves with a control point
+#' @param start_pos Vector2. Start position.
+#' @param end_pos Vector2. End position.
+#' @param control_pos Vector2. Control points.
+#' @param thick Float value defining thickness.
+#' @param color Color object. Integer vector with RGBA values between 0 and 255.
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+draw_line_bezier_quad <- function(start_pos, end_pos, control_pos, thick, color) {
+        .Call("DrawLineBezierQuad_R", start_pos, end_pos, control_pos, thick, color)
+        invisible()
+}
+
+#' @family Basic shapes drawing functions
+#' @title Draw line using cubic bezier curves with 2 control points
+#' @param start_pos Vector2. Start position.
+#' @param end_pos Vector2. End position.
+#' @param control_pos Vector2. Control points.
+#' @param thick Float value defining thickness.
+#' @param color Color object. Integer vector with RGBA values between 0 and 255.
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+draw_line_bezier_cubic <- function(start_pos, end_pos, control_pos, thick, color) {
+        .Call("DrawLineBezierCubic_R", start_pos, end_pos, control_pos, thick, color)
+        invisible()
+}
+
+#' @family Basic shapes drawing functions
+#' @title Draw lines sequence
+#' @param points List with Vector2 elements.
+#' @param color Color object. Integer vector with RGBA values between 0 and 255.
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+draw_line_strip <- function(points, color) {
+        .Call("DrawLineStrip_R", points, length(points), color)
+        invisible()
+}
--- src/shapes.h
+++ src/shapes.h
@@ -4,7 +4,12 @@
 #include <R.h>
 #include <Rinternals.h>
 
+// Set texture and rectangle to be used on shapes drawing
+// NOTE: It can be useful when using basic shapes and one single font,
+// defining a font char white rectangle would allow drawing everything in a single draw call
 SEXP SetShapesTexture_R(SEXP texture, SEXP rectangle_source);
+
+// Basic shapes drawing functions
 SEXP DrawPixel_R(SEXP posX, SEXP posY, SEXP color);
 SEXP DrawPixelV_R(SEXP vector2_position, SEXP color);
 SEXP DrawLine_R(SEXP position, SEXP color);
