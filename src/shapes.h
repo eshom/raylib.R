7e8726d186ab4e8260f89eabceb9cec83b0a9251
eshom <48328872+eshom@users.noreply.github.com>
2022-04-09 17:23:39 +0300

    Updated shape headers and one-liner to get all declarations

--- src/shapes.h
+++ src/shapes.h
@@ -7,5 +7,12 @@
 SEXP SetShapesTexture_R(SEXP texture, SEXP rectangle_source);
 SEXP DrawPixel_R(SEXP posX, SEXP posY, SEXP color);
 SEXP DrawPixelV_R(SEXP vector2_position, SEXP color);
+SEXP DrawLine_R(SEXP position, SEXP color);
+SEXP DrawLineV_R(SEXP start_pos, SEXP end_pos, SEXP color);
+SEXP DrawLineEx_R(SEXP start_pos, SEXP end_pos, SEXP thick, SEXP color);
+SEXP DrawLineBezier_R(SEXP start_pos, SEXP end_pos, SEXP thick, SEXP color);
+SEXP DrawLineBezierQuad_R(SEXP start_pos, SEXP end_pos, SEXP control_pos, SEXP thick, SEXP color);
+SEXP DrawLineBezierCubic_R(SEXP start_pos, SEXP end_pos, SEXP start_control_pos, SEXP end_control_pos, SEXP thick, SEXP color);
+SEXP DrawLineStrip_R(SEXP points, SEXP point_count, SEXP color);
 
 #endif
