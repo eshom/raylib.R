26ccceb432c105accc48239756a59784ecd1ee41
eshom <48328872+eshom@users.noreply.github.com>
2022-04-21 18:23:14 +0300

    Autogenerate TitleCase bindings

---
+++ R/titlecase_autogen.R
@@ -0,0 +1,483 @@
+###########################################################
+#The following file is auto generated, do not edit by hand#
+###########################################################
+#' @rdname init_window 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+InitWindow <- init_window 
+
+#' @rdname window_should_close 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+WindowShouldClose <- window_should_close 
+
+#' @rdname close_window 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CloseWindow <- close_window 
+
+#' @rdname is_window_ready 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowReady <- is_window_ready 
+
+#' @rdname is_window_fullscreen 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowFullscreen <- is_window_fullscreen 
+
+#' @rdname is_window_hidden 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowHidden <- is_window_hidden 
+
+#' @rdname is_window_minimized 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowMinimized <- is_window_minimized 
+
+#' @rdname is_window_maximized 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowMaximized <- is_window_maximized 
+
+#' @rdname is_window_focused 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowFocused <- is_window_focused 
+
+#' @rdname is_window_resized 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowResized <- is_window_resized 
+
+#' @rdname is_window_state 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsWindowState <- is_window_state 
+
+#' @rdname set_window_state 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetWindowState <- set_window_state 
+
+#' @rdname clear_window_state 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+ClearWindowState <- clear_window_state 
+
+#' @rdname toggle_fullscreen 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+ToggleFullscreen <- toggle_fullscreen 
+
+#' @rdname clear_background 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+ClearBackground <- clear_background 
+
+#' @rdname begin_drawing 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+BeginDrawing <- begin_drawing 
+
+#' @rdname end_drawing 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+EndDrawing <- end_drawing 
+
+#' @rdname begin_mode_2d 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+BeginMode2d <- begin_mode_2d 
+
+#' @rdname end_mode_2d 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+EndMode2d <- end_mode_2d 
+
+#' @rdname begin_mode_3d 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+BeginMode3d <- begin_mode_3d 
+
+#' @rdname end_mode_3d 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+EndMode3d <- end_mode_3d 
+
+#' @rdname set_target_fps 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetTargetFps <- set_target_fps 
+
+#' @rdname is_key_pressed 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsKeyPressed <- is_key_pressed 
+
+#' @rdname is_key_down 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsKeyDown <- is_key_down 
+
+#' @rdname is_key_released 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsKeyReleased <- is_key_released 
+
+#' @rdname is_key_up 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsKeyUp <- is_key_up 
+
+#' @rdname set_exit_key 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetExitKey <- set_exit_key 
+
+#' @rdname get_key_pressed 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetKeyPressed <- get_key_pressed 
+
+#' @rdname get_char_pressed 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetCharPressed <- get_char_pressed 
+
+#' @rdname is_mouse_button_pressed 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsMouseButtonPressed <- is_mouse_button_pressed 
+
+#' @rdname is_mouse_button_down 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsMouseButtonDown <- is_mouse_button_down 
+
+#' @rdname is_mouse_button_released 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsMouseButtonReleased <- is_mouse_button_released 
+
+#' @rdname is_mouse_button_up 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+IsMouseButtonUp <- is_mouse_button_up 
+
+#' @rdname get_mouse_x 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetMouseX <- get_mouse_x 
+
+#' @rdname get_mouse_y 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetMouseY <- get_mouse_y 
+
+#' @rdname get_mouse_position 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetMousePosition <- get_mouse_position 
+
+#' @rdname get_mouse_delta 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetMouseDelta <- get_mouse_delta 
+
+#' @rdname set_mouse_position 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetMousePosition <- set_mouse_position 
+
+#' @rdname set_mouse_offset 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetMouseOffset <- set_mouse_offset 
+
+#' @rdname set_mouse_scale 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetMouseScale <- set_mouse_scale 
+
+#' @rdname get_mouse_wheel_move 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetMouseWheelMove <- get_mouse_wheel_move 
+
+#' @rdname set_mouse_cursor 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetMouseCursor <- set_mouse_cursor 
+
+#' @rdname set_shapes_texture 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+SetShapesTexture <- set_shapes_texture 
+
+#' @rdname draw_pixel 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawPixel <- draw_pixel 
+
+#' @rdname draw_pixel_v 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawPixelV <- draw_pixel_v 
+
+#' @rdname draw_line 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawLine <- draw_line 
+
+#' @rdname draw_line_v 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawLineV <- draw_line_v 
+
+#' @rdname draw_line_ex 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawLineEx <- draw_line_ex 
+
+#' @rdname draw_line_bezier 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawLineBezier <- draw_line_bezier 
+
+#' @rdname draw_line_bezier_quad 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawLineBezierQuad <- draw_line_bezier_quad 
+
+#' @rdname draw_line_bezier_cubic 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawLineBezierCubic <- draw_line_bezier_cubic 
+
+#' @rdname draw_line_strip 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawLineStrip <- draw_line_strip 
+
+#' @rdname draw_circle 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCircle <- draw_circle 
+
+#' @rdname draw_circle_sector 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCircleSector <- draw_circle_sector 
+
+#' @rdname draw_circle_sector_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCircleSectorLines <- draw_circle_sector_lines 
+
+#' @rdname draw_circle_gradient 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCircleGradient <- draw_circle_gradient 
+
+#' @rdname draw_circle_v 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCircleV <- draw_circle_v 
+
+#' @rdname draw_circle_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCircleLines <- draw_circle_lines 
+
+#' @rdname draw_ellipse 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawEllipse <- draw_ellipse 
+
+#' @rdname draw_ellipse_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawEllipseLines <- draw_ellipse_lines 
+
+#' @rdname draw_ring 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRing <- draw_ring 
+
+#' @rdname draw_ring_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRingLines <- draw_ring_lines 
+
+#' @rdname draw_rectangle 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangle <- draw_rectangle 
+
+#' @rdname draw_rectangle_v 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleV <- draw_rectangle_v 
+
+#' @rdname draw_rectangle_rec 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleRec <- draw_rectangle_rec 
+
+#' @rdname draw_rectangle_pro 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectanglePro <- draw_rectangle_pro 
+
+#' @rdname draw_rectangle_gradient_v 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleGradientV <- draw_rectangle_gradient_v 
+
+#' @rdname draw_rectangle_gradient_h 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleGradientH <- draw_rectangle_gradient_h 
+
+#' @rdname draw_rectangle_gradient_ex 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleGradientEx <- draw_rectangle_gradient_ex 
+
+#' @rdname draw_rectangle_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleLines <- draw_rectangle_lines 
+
+#' @rdname draw_rectangle_lines_ex 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleLinesEx <- draw_rectangle_lines_ex 
+
+#' @rdname draw_rectangle_rounded 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleRounded <- draw_rectangle_rounded 
+
+#' @rdname draw_rectangle_rounded_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawRectangleRoundedLines <- draw_rectangle_rounded_lines 
+
+#' @rdname draw_triangle 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawTriangle <- draw_triangle 
+
+#' @rdname draw_triangle_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawTriangleLines <- draw_triangle_lines 
+
+#' @rdname draw_triangle_fan 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawTriangleFan <- draw_triangle_fan 
+
+#' @rdname draw_triangle_strip 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawTriangleStrip <- draw_triangle_strip 
+
+#' @rdname draw_poly 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawPoly <- draw_poly 
+
+#' @rdname draw_poly_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawPolyLines <- draw_poly_lines 
+
+#' @rdname draw_poly_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawPolyLines <- draw_poly_lines 
+
+#' @rdname check_collision_recs 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionRecs <- check_collision_recs 
+
+#' @rdname check_collision_circles 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionCircles <- check_collision_circles 
+
+#' @rdname check_collision_circle_rec 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionCircleRec <- check_collision_circle_rec 
+
+#' @rdname check_collision_point_rec 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionPointRec <- check_collision_point_rec 
+
+#' @rdname check_collision_point_circle 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionPointCircle <- check_collision_point_circle 
+
+#' @rdname check_collision_point_triangle 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionPointTriangle <- check_collision_point_triangle 
+
+#' @rdname check_collision_lines 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionLines <- check_collision_lines 
+
+#' @rdname check_collision_point_line 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+CheckCollisionPointLine <- check_collision_point_line 
+
+#' @rdname get_collision_rec 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+GetCollisionRec <- get_collision_rec 
+
+#' @rdname draw_fps 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawFps <- draw_fps 
+
+#' @rdname draw_text 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawText <- draw_text 
+
+#' @rdname draw_cube 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCube <- draw_cube 
+
+#' @rdname draw_cube_v 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCubeV <- draw_cube_v 
+
+#' @rdname draw_cube_wires 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCubeWires <- draw_cube_wires 
+
+#' @rdname draw_cube_wires_v 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawCubeWiresV <- draw_cube_wires_v 
+
+#' @rdname draw_grid 
+#' @useDynLib raylib.R, .registration = TRUE
+#' @export
+DrawGrid <- draw_grid 
+
