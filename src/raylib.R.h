0a83dcacd711ef7a0ef014db4800f82853bed24c
eshom <48328872+eshom@users.noreply.github.com>
2022-05-05 09:47:00 +0300

    Added header to main package header

--- src/raylib.R.h
+++ src/raylib.R.h
@@ -13,6 +13,7 @@
 #include "camera3d.h"
 #include "textures.h"
 #include "rcamera.h"
+#include "image.h"
 
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
